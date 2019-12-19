7186d7526023ce2502561c812b10633f71fecd66
passion-logan <wangdadamailbox@163.com>
2019-12-19 16:13:21 +0800

    添加分布式链路追踪

--- trace-2/src/main/java/com/cody/Trace2Application.java
+++ trace-2/src/main/java/com/cody/Trace2Application.java
@@ -2,9 +2,25 @@ package com.cody;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
+@RestController
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Trace2Application {
+
+    private final Logger logger = Logger.getLogger(String.valueOf(getClass()));
+
+    @RequestMapping(value = "/trace-2", method = RequestMethod.GET)
+    public String trace() {
+        logger.info("===<call trace-2>===");
+        return "Trace";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Trace2Application.class, args);
