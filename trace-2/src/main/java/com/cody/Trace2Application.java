a4d2e1e1f9e46a11cdc6f5618bf52eda84cd403c
passion-logan <wangdadamailbox@163.com>
2019-12-19 16:11:27 +0800

    添加分布式链路追踪

---
+++ trace-2/src/main/java/com/cody/Trace2Application.java
@@ -0,0 +1,13 @@
+package com.cody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trace2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trace2Application.class, args);
+    }
+
+}
